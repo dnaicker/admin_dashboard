b3265e0e21f0ca0dd4fbff5a7683154e786de7d3
Denver <dnaicker@users.noreply.github.com>
2023-01-30 13:53:38 +0200

    updates

--- public/js/env.js
+++ public/js/env.js
@@ -1,5 +1,3 @@
 export async function ngrok() {
-	return "http://6647-105-186-219-72.ngrok.io";
+	return "http://bc10-105-184-70-48.ngrok.io";
 }
-
-// const auth_token = "CiVodHRwczovL3RyaW5zaWMuaWQvc2VjdXJpdHkvdjEvb2Jlcm9uEkkKKnVybjp0cmluc2ljOndhbGxldHM6N1VwRmtIUEdvektWUWNFSHVLYVZ3TSIbdXJuOnRyaW5zaWM6ZWNvc3lzdGVtczpDU0lSGjCTwP0t3e2BdAKnkSjJIJN1HMwlexAmvYBUGBzR_DEFkGZebj-IdHu48JKhMrjBdegiAA"
