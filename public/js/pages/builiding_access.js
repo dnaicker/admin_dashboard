0e86bef32d7aa5d7eaab5444b56fae085b4dba10
Denver <dnaicker@users.noreply.github.com>
2023-02-08 17:50:53 +0200

    adding buildings dynamically

--- public/js/pages/builiding_access.js
+++ public/js/pages/builiding_access.js
@@ -11,7 +11,8 @@ $(document).ready(function () {
 	$("#modal_load").load("../modal.html");
 	$("#navigation_load").load("../navigation.html");
 	
-	generate_qr_code("urn:template:CSIR:csir-building-access-card-d54c3e94-867d-48fe-a9e7-32526b838c6f/", '[{"building_number": "43"}]')
+	generate_building_qr_code("urn:template:CSIR:csir-building-access-card-d54c3e94-867d-48fe-a9e7-32526b838c6f/", '[{"building_number": "43"}]', 43)
+	generate_building_qr_code("urn:template:CSIR:csir-building-access-card-d54c3e94-867d-48fe-a9e7-32526b838c6f/", '[{"building_number": "9"}]', 9)
 
 	
 	// setinterval check verification status
@@ -32,6 +33,14 @@ $(document).ready(function () {
 						// lookUp - uuid
 						// fieldsAndValuesRequired for lookup
 			// sample request values
-async function generate_qr_code(template_id, fields_and_values_required) {
-	$("#show_qr_code")[0].innerHTML = `<p><img src='https://chart.googleapis.com/chart?cht=qr&chl=/${ngrok_url}/createVerificationQRCodeLookup/insert_wallet_holder_auth_token/${template_id}/${fields_and_values_required}&chs=200x200&chld=L|1' alt='qr code' /><p>`;
+async function generate_building_qr_code(template_id, fields_and_values_required, building_number) {
+	let arr = []
+
+	arr.push('<h4>Building <span id="building_number">'+ building_number +'</span></h4>')
+	arr.push(`<p><img src='https://chart.googleapis.com/chart?cht=qr&chl=/${ngrok_url}/createVerificationQRCodeLookup/insert_wallet_holder_auth_token/${template_id}/${fields_and_values_required}&chs=200x200&chld=L|1' alt='qr code' /><p>`)
+	arr.push('<p class="access_details">Please scan the following QR code to verify you have access to building <span id="building_number">'+ building_number +'</span>.</p>')
+
+	$("#show_qr_code").append(arr.join(''));
+
+
 }
