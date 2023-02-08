7107531ad375c2e773f93451191fcc5c5e829b1a
Denver <dnaicker@users.noreply.github.com>
2023-02-08 17:40:01 +0200

    display qr code

--- public/js/pages/builiding_access.js
+++ public/js/pages/builiding_access.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
 	$("#modal_load").load("../modal.html");
 	$("#navigation_load").load("../navigation.html");
 	
-
+	generate_qr_code("urn:template:CSIR:csir-building-access-card-d54c3e94-867d-48fe-a9e7-32526b838c6f/", '[{"building_number": "43"}]')
 
 	
 	// setinterval check verification status
@@ -31,19 +31,7 @@ $(document).ready(function () {
 						// status - pending
 						// lookUp - uuid
 						// fieldsAndValuesRequired for lookup
-async function generate_qr_code() {
-	$.ajax({
-		url: `${ngrok_url}/trinsicCreateOrLoginAccount/${email}`,
-		type: "GET",
-		success: function (result) {
-			console.log(result);
-			challenge = result;
-			$("#cover-spin").hide();
-			alert("Email has been sent.");
-		},
-		error: function (error) {
-			$("#cover-spin").hide();
-			console.log(error);
-		}
-	});
+			// sample request values
+async function generate_qr_code(template_id, fields_and_values_required) {
+	$("#show_qr_code")[0].innerHTML = `<p><img src='https://chart.googleapis.com/chart?cht=qr&chl=/${ngrok_url}/createVerificationQRCodeLookup/wallet_holder_auth_token/${template_id}/${fields_and_values_required}&chs=200x200&chld=L|1' alt='qr code' /><p>`;
 }
