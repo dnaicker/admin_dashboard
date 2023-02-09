1c2840288074723afe1afc1ff32fd9c23babb0c0
Denver <dnaicker@users.noreply.github.com>
2023-02-09 15:57:48 +0200

    updates

--- public/js/pages/builiding_access.js
+++ public/js/pages/builiding_access.js
@@ -11,10 +11,8 @@ $(document).ready(function () {
 	$("#modal_load").load("../modal.html");
 	$("#navigation_load").load("../navigation.html");
 	
-	generate_building_qr_code('{"api": "/createVerificationQRCodeLookup/{authToken}/{templateId}/{fieldsAndValuesRequired}", "authToken": "you provide", "template_id": "urn:template:CSIR:csir-building-access-card-d54c3e94-867d-48fe-a9e7-32526b838c6f/", "fieldsAndValuesRequired": "[{\"building_number\": \"43\"}]"}', 43 );
-	generate_building_qr_code('{"api": "/createVerificationQRCodeLookup/{authToken}/{templateId}/{fieldsAndValuesRequired}", "authToken": "you provide", "template_id": "urn:template:CSIR:csir-building-access-card-d54c3e94-867d-48fe-a9e7-32526b838c6f/", "fieldsAndValuesRequired": "[{\"building_number\": \"9\"}]"}', 9 );
-
-
+	generate_building_qr_code("urn:template:CSIR:csir-building-access-card-d54c3e94-867d-48fe-a9e7-32526b838c6f/", '[{"building_number": "43"}]', 43)
+	generate_building_qr_code("urn:template:CSIR:csir-building-access-card-d54c3e94-867d-48fe-a9e7-32526b838c6f/", '[{"building_number": "9"}]', 9)
 
 	// setinterval check verification status
 				// get verification status based on id
@@ -35,11 +33,11 @@ $(document).ready(function () {
 						// fieldsAndValuesRequired for lookup
 			// return template id and lookup id
 
-async function generate_building_qr_code(requirements, building_number) {
+async function generate_building_qr_code(template_id, fields_and_values_required, building_number) {
 	let arr = []
 
 	arr.push('<h4>Building <span id="building_number">'+ building_number +'</span></h4>')
-	arr.push(`<p><img src='https://chart.googleapis.com/chart?cht=qr&chl=/${ngrok_url}/createMobileAppScanQRCodeURLDetails/${requirements}&chs=200x200&chld=L|1' alt='qr code' /><p>`)
+	arr.push(`<p><img src='https://chart.googleapis.com/chart?cht=qr&chl=/${ngrok_url}/createVerificationQRCodeLookup/${template_id}/${fields_and_values_required}&chs=200x200&chld=L|1' alt='qr code' /><p>`)
 	arr.push('<p class="access_details">Please scan the following QR code to verify you have access to building <span id="building_number">'+ building_number +'</span>.</p>')
 
 	$("#show_qr_code").append(arr.join(''));
